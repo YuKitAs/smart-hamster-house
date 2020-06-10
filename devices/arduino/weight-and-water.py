d4a350073201d5625a125d9015fc3524add78604
YuKitAs <moncher.merci@gmail.com>
2020-06-10 03:10:21 +0200

    Modify logic for recording weight, some refactoring

--- devices/arduino/weight-and-water.py
+++ devices/arduino/weight-and-water.py
@@ -10,15 +10,25 @@ logging.basicConfig(filename='logs/weight-and-water.log', format='%(asctime)s - 
                     level='DEBUG')
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+
+# InfluxDB
 client = InfluxDBClient('localhost', 8086)
+db_water = "water"
+db_weight = "weight"
+measurement_weight = "weight"
+tag_hamster_weight = "hamster_weight"
+tag_tare_weight = "tare_weight"
 
 num_read_weights = 5
-tare_weight = 165
-interval = 86400
+tare_weight_threshold = 150
+tare_weight_default = 165
+time_default = 0
+write_interval = 86400
+read_interval = 3600
 
 
 def write_water_alarm():
-    client.switch_database('water')
+    client.switch_database(db_water)
 
     json_body = [{
         "measurement": "level",
@@ -37,14 +47,15 @@ def valid_weights(values):
     return statistics.stdev(values) < 1
 
 
-def write_weight(value):
+def write_weight(tag, value):
     if value:
-        client.switch_database('weight')
+        client.switch_database(db_weight)
 
         json_body = [{
-            "measurement": "weight",
+            "measurement": measurement_weight,
             "tags": {
-                "location": "home"
+                "location": "home",
+                "type": tag
             },
             "fields": {
                 "value": value
@@ -54,8 +65,16 @@ def write_weight(value):
         client.write_points(json_body)
 
 
-read_weights = []
-weight_update_time = 0
+def read_last_weight(tag):
+    client.switch_database(db_weight)
+    query_result = client.query("SELECT last(*) FROM {} WHERE type = '{}'".format(measurement_weight, tag))
+    logging.debug("Last weight query result for tag '{}': {}".format(tag, query_result))
+    return next(query_result.get_points())
+
+
+read_hamster_weights = []
+read_tare_weights = []
+last_read_time = 0
 
 while True:
     try:
@@ -63,27 +82,57 @@ while True:
         logging.debug("Serial: {}".format(raw_value))  # e.g. {"liquid_alarm":false,"weight":0.005319}
 
         json_value = json.loads(raw_value)
-        logging.debug(json_value)
 
         if json_value['liquid_alarm']:
-            logging.debug('Writing water alarm')
+            logging.debug('[Water] Writing water alarm')
             write_water_alarm()
 
         weight = json_value['weight']
-        if weight > (tare_weight + 20) and (time.time() - weight_update_time) >= interval:
-            if len(read_weights) < num_read_weights:
-                read_weights.append(weight - tare_weight)
-                continue
-            else:
-                if valid_weights(read_weights):
-                    logging.debug('Writing weight: {}'.format(statistics.mean(read_weights)))
-                    write_weight(statistics.mean(read_weights))
-                    read_weights = []
+        current_time = time.time()
 
-                    current_time = time.time()
-                    logging.debug('Updating time: {}'.format(time.ctime(current_time)))
-                    weight_update_time = current_time
+        if weight > tare_weight_threshold and current_time - last_read_time >= read_interval:
+            last_tare_weight = read_last_weight(tag_tare_weight)
+
+            last_tare_weight_update_time = time.mktime(
+                time.strptime(last_tare_weight['time'][:19], "%Y-%m-%dT%H:%M:%S")) \
+                if last_tare_weight is not None else time_default
+
+            last_tare_weight_value = last_tare_weight['last_value'] \
+                if last_tare_weight is not None else tare_weight_default
+
+            last_hamster_weight = read_last_weight(tag_hamster_weight)
+            last_hamster_weight_update_time = time.mktime(
+                time.strptime(last_hamster_weight['time'][:19], "%Y-%m-%dT%H:%M:%S")) \
+                if last_hamster_weight is not None else time_default
+
+            if 1 < abs(weight - last_tare_weight_value) <= 20:  # check tare weight
+                if len(read_tare_weights) < num_read_weights:
+                    read_tare_weights.append(weight)
+                    continue
                 else:
-                    read_weights = read_weights[1:]
+                    if valid_weights(read_tare_weights):
+                        mean_value = statistics.mean(read_tare_weights)
+                        logging.debug('[Weight] Writing tare weight: {}'.format(mean_value))
+                        write_weight(tag_tare_weight, mean_value)
+
+                        read_tare_weights = []
+                        last_read_time = time.time()
+                    else:
+                        read_tare_weights = read_tare_weights[1:]
+            elif abs(weight - last_tare_weight_value) > 20 \
+                    and current_time - last_hamster_weight_update_time > write_interval:  # check hamster weight
+                if len(read_hamster_weights) < num_read_weights:
+                    read_hamster_weights.append(weight - last_tare_weight_value)
+                    continue
+                else:
+                    if valid_weights(read_hamster_weights):
+                        mean_value = statistics.mean(read_hamster_weights)
+                        logging.debug('[Weight] Writing hamster weight: {}'.format(mean_value))
+                        write_weight(tag_hamster_weight, mean_value)
+
+                        read_hamster_weights = []
+                        last_read_time = time.time()
+                    else:
+                        read_hamster_weights = read_hamster_weights[1:]
     except Exception as ex:
         logging.error(ex)
